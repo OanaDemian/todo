39ffa0e6a3fb57e9631fbe8a95224485a3d472c5
Oana Demian <oana.demian@gmail.com>
2025-10-16 22:40:44 +0100

    test raise exception  when todo_list is trying to add a todo with empty task

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -1,3 +1,4 @@
+import pytest
 from lib.todo_list import TodoList
 from unittest.mock import Mock
 """
@@ -10,3 +11,17 @@ def test_adds_one_todo_in_list():
     todo = Mock()
     todo_list.add(todo)
     assert todo_list.get_todos() == [todo]
+
+
+"""
+When we add one todo with an empty task
+We get an error
+"""
+
+def test_adds_one_todo_with_empty_task_raises_exception():
+    todo_list = TodoList()
+    todo = Mock()
+    todo.task = ""
+    with pytest.raises(Exception) as e:
+        todo_list.add(todo)
+    assert str(e.value) == "Cannot add empty task to the tasklist"
