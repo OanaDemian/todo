6d5d8e997289ffc5f9615a7e42f7fed073140be5
Oana Demian <oana.demian@gmail.com>
2025-10-16 23:28:16 +0100

    adds unit test for #complete which returns the list of complete todos

--- tests/test_todo_list.py
+++ tests/test_todo_list.py
@@ -48,3 +48,26 @@ def test_todolist_incomplete():
     todo_list.add(todo_3)
     todo_1.complete = True
     assert todo_list.incomplete() == [todo_2, todo_3]
+
+"""
+When we 3 todos
+Then mark one as complete
+#complete returns a list of the completed todo
+"""
+
+def test_todolist_complete():
+    todo_list = TodoList()
+    todo_1 = Mock("Feed the tarantula!")
+    todo_2 = Mock("Buy more food for the spider")
+    todo_3 = Mock("Clean the spider's den")
+    todo_1.task = "Feed the tarantula!"
+    todo_2.task = "Buy more food for the spider"
+    todo_3.task = "Clean the spider's de"
+    todo_1.complete = False
+    todo_2.complete = False
+    todo_3.complete = False
+    todo_list.add(todo_1)
+    todo_list.add(todo_2)
+    todo_list.add(todo_3)
+    todo_1.complete = True
+    assert todo_list.complete() == [todo_1]
